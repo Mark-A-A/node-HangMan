f61d00c91b506d0ea36312dc6ae949ebb0f66e49
Mark-A-A <markawai01@gmail.com>
2016-01-26 18:24:34 -0500

    testing and debugging

--- word.js
+++ word.js
@@ -20,7 +20,7 @@ function Word(wrd) {
       
     }
     console.log(this.lets); // Print array of word letters
-    return this.lets;
+    // return this.lets;
   },
 
   this.checkIfLetterFound = function(guessLetter){
@@ -41,8 +41,8 @@ function Word(wrd) {
 
   this.didWeFindTheWord = function(){
     debugger
-    this.lets.every = function(curLet) {
-      console.log(curLet)
+    this.lets.every = function(curLet) { //curLet = current letter
+      console.log(curLet);
       if(curLet.appear === true){
        this.found = true; 
       }
@@ -53,24 +53,27 @@ function Word(wrd) {
   this.wordRender = function WordRender() {   
     debugger                                                                                                                                      
       //create a variable called str set it to "" empty string                                                                                                                  
-      var str = '';
-      //it will run a loop that iterate over each object in let array
-      //everytime the loop iterates, calling the letterRender on that object and then string 
-      //concatenate that to the varaible str
+    var str = '';
+    //it will run a loop that iterate over each object in let array
+    //everytime the loop iterates, calling the letterRender on that object and then string 
+    //concatenate that to the varaible str
 
-      for (var i= 0; i <this.lets.length; i++){
-        str += Letter.letterRender(i);
-      }
-      return str
+    for (var i= 0; i <this.lets.length; i++){
+      console.log( Letter.letterRender( this.lets[i] ));
+      str += letter.letterRender(this.lets[i]);
+      console.log(str);
+    }
+    return str
     
   }
 }; //End of Word object
 
-var newWord = new Word("apple");
-console.log(newWord);
+module.exports = Word; 
+//var newWord = new Word("apple");  This works
+//console.log(newWord);
 
-var wordLetters = newWord.getLets();
-console.log(wordLetters);
+//var wordLetters = newWord.getLets();
+//console.log(wordLetters);
 
-var letterCheckIfFound = newWord.checkIfLetterFound("v");
-console.log(letterCheckIfFound);
+//var letterCheckIfFound = newWord.checkIfLetterFound("v");
+//console.log(letterCheckIfFound);
